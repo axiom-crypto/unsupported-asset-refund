067b32acf787390f02ed85db0a3dfdc208c96b7e
Yi Sun <yi-sun@users.noreply.github.com>
2024-02-15 11:06:33 -0500

    feat: update deploy script

--- script/UnsupportedAssetRefund.s.sol
+++ script/UnsupportedAssetRefund.s.sol
@@ -3,24 +3,25 @@ pragma solidity 0.8.19;
 
 import { Script, console2 } from "forge-std/Script.sol";
 
+import { AxiomV2Addresses } from "@axiom-crypto/axiom-std/AxiomV2Addresses.sol";
+
 import { UnsupportedAssetRefund } from "../src/UnsupportedAssetRefund.sol";
 
 contract UnsupportedAssetRefundScript is Script {
-    address public constant AXIOM_V2_QUERY_MOCK_SEPOLIA_ADDR = 0x83c8c0B395850bA55c830451Cfaca4F2A667a983;
+    address public AXIOM_V2_QUERY_MOCK_SEPOLIA_ADDR;
     bytes32 _querySchema;
 
     function setUp() public {
         string memory artifact = vm.readFile("./app/axiom/data/compiled.json");
         _querySchema = bytes32(vm.parseJson(artifact, ".querySchema"));
+
+        AXIOM_V2_QUERY_MOCK_SEPOLIA_ADDR = AxiomV2Addresses.axiomV2QueryMockAddress(11_155_111);
     }
 
     function run() public {
         vm.startBroadcast();
 
-        new UnsupportedAssetRefund(
-            AXIOM_V2_QUERY_MOCK_SEPOLIA_ADDR,
-            _querySchema
-        );
+        new UnsupportedAssetRefund(AXIOM_V2_QUERY_MOCK_SEPOLIA_ADDR, _querySchema);
 
         vm.stopBroadcast();
     }
